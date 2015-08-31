87b0901998fce1d8a77ef306c765b74d9b3eb053
Christine Lin <chlin@vassar.edu>
2015-08-31 17:22:36 -0400

    First snapshot

---
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author Christine Lin
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     * Constructs a Greeter object that can greet a person or entity.
+         @param aName the name of the person or entity who should
+         be addressed in the greetings.
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World!");
+    }
+}
+
